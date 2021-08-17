ebc12c69a1d6823d5ccdc962e66256beaedc601f
Marcus Edralin <medralin36@yahoo.com>
2021-08-16 17:33:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import User from './components/User';
+
+function App() {
+  return (
+    <User />
+  );
+}
+
+export default App;
---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Card from './Card';
+import Button from './Button';
+
+const User = () => {
+    return (
+        <Card>
+            <Button />
+        </Card>
+    )
+}
+
+export default User
